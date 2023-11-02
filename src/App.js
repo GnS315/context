2a3c8ad42da7edb1bf4fb1876b39a6ab6279df24
Egor Morozov <e.d.morozov@esoft.local>
2023-11-02 17:28:21 +0500

    context

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React, {useContext, useState} from 'react';
+// Импортируем контекст
 import {AuthContext} from "./context";
-
+/*
+* В зависимости от авторизации будет показываться надпись "Авторизован"/"Не авторизован"
+* Авторизация происходит по нажатию на кнопку
+* */
 function App() {
   const [isAuth, setIsAuth] = useState(false)
   const changeAuth = () => setIsAuth(!isAuth)
+  const buttonValue = isAuth ? 'Выйти' : 'Авторизоваться'
 
   return (
     <AuthContext.Provider value = {{isAuth, setIsAuth}}>
       <Page />
-      <button onClick={changeAuth}>Кнопка</button>
+      <Page2 />
+      <button onClick={changeAuth}>{buttonValue}</button>
     </AuthContext.Provider>
   )
 }
+/*
+* Использовать контекст можно при помощи хука useContext()
+* Или при помощи AuthContext.Consumer
+* */
 
 const Page = () => {
   const {isAuth} = useContext(AuthContext)
@@ -21,4 +31,12 @@ const Page = () => {
       <h1>Не авторизован</h1>
   )
 }
+const Page2 = () => {
+  return (
+    <AuthContext.Consumer>
+      {value => value.isAuth ? <h1>Авторизован2</h1> : <h1>Не авторизован2</h1>}
+    </AuthContext.Consumer>
+  )
+}
+
 export default App;
--- src/context/index.js
+++ src/context/index.js
@@ -1,3 +1,5 @@
 import {createContext} from "react";
-
+/*
+* Создаем контекст с дефолтным состоянием
+* */
 export const AuthContext = createContext(null)
